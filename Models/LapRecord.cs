d5465218439de9a74b0c77e36966443fc44f3ce1
Владимир Ушаков <svayznoy@inbox.ru>
2025-06-09 19:30:39 +0700

    Реализовать модель, представляющую прохождение круга одним участником

---
+++ Models/LapRecord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace RunningEventTracker.Models
+{
+    /// <summary>
+    /// Представляет один зарегистрированный круг участия бегуна.
+    /// </summary>
+    public class LapRecord
+    {
+        /// <summary>
+        /// Идентификатор участника забега.
+        /// </summary>
+        public int Participant { get; set; }
+
+        /// <summary>
+        /// Общее время прохождения кругов в секундах.
+        /// </summary>
+        public double TotalSeconds { get; set; }
+
+        /// <summary>
+        /// Порядковый номер круга (начиная с 1).
+        /// </summary>
+        public int LapNumber { get; set; }
+
+        /// <summary>
+        /// Дата и время регистрации круга.
+        /// </summary>
+        public DateTime RecordTime { get; set; }
+    }
+}
