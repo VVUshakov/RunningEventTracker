a7ea4477981c93405628de81699765b7421710d1
Владимир Ушаков <svayznoy@inbox.ru>
2025-06-09 19:42:36 +0700

    Реализовать класс хранилища исторических записей действий пользователей

---
+++ Services/ActionHistory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+namespace RunningEventTracker.Services
+{
+    /// <summary>
+    /// Служба для хранения недавней истории действий.
+    /// </summary>
+    public class ActionHistory : IActionHistory
+    {
+        private readonly Queue<string> _logs = new Queue<string>(5);
+
+        /// <summary>
+        /// Создает новую запись в журнале.
+        /// </summary>
+        public void Log(string message)
+        {
+            _logs.Enqueue(message);
+            if(_logs.Count > 5)
+                _logs.Dequeue();
+        }
+
+        /// <summary>
+        /// Возврат списка недавних записей.
+        /// </summary>
+        public IEnumerable<string> RecentLogs()
+        {
+            return _logs.Reverse().AsEnumerable();
+        }
+    }
+}
