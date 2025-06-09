a4dda9a80d06534abb0f8d617a6eddd976867074
Владимир Ушаков <svayznoy@inbox.ru>
2025-06-09 19:37:27 +0700

    Реализовать таймер

---
+++ Services/RaceTimer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Timers;
+
+namespace RunningEventTracker.Services
+{
+    /// <summary>
+    /// Реализует функциональность обратного отсчёта.
+    /// </summary>
+    public class RaceTimer : IRaceTimer
+    {
+        private readonly Timer _timer;
+
+        public RaceTimer()
+        {
+            _timer = new Timer(1000); // интервал обновлений раз в секунду
+            _timer.Tick += OnTick;
+        }
+
+        /// <summary>
+        /// Триггерированное событие при каждом новом тике таймера.
+        /// </summary>
+        public event EventHandler<EventArgs> Tick;
+
+        /// <summary>
+        /// Метод, вызванный при наступлении очередного интервала.
+        /// </summary>
+        protected virtual void OnTick(object sender, EventArgs args)
+        {
+            Tick?.Invoke(this, args);
+        }
+
+        /// <summary>
+        /// Начало отсчёта таймера.
+        /// </summary>
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        /// <summary>
+        /// Завершение работы таймера.
+        /// </summary>
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
