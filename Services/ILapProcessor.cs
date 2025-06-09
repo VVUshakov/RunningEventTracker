2914a175b0ab21af3a5ebc819e3020bd9294afad
Владимир Ушаков <svayznoy@inbox.ru>
2025-06-09 19:38:22 +0700

    Реализовать интерфейс для обработки прохождения кругов участниками

---
+++ Services/ILapProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace RunningEventTracker.Services
+{
+    /// <summary>
+    /// Интерфейс для сервиса обработки зарегистрированных кругов.
+    /// </summary>
+    public interface ILapProcessor
+    {
+        /// <summary>
+        /// Регистрация нового круга для указанного участника.
+        /// </summary>
+        void ProcessLap(int participantId);
+
+        /// <summary>
+        /// Возвращает список всех зарегистрированных кругов.
+        /// </summary>
+        IEnumerable<LapRecord> GetAllLaps();
+    }
+}
