928d0995a9338d62446ba4a274e1f6dccd7afab6
Владимир Ушаков <svayznoy@inbox.ru>
2025-06-09 19:34:19 +0700

    Реализоват интерфейс для управления таймером

---
+++ Services/IRaceTimer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RunningEventTracker.Services
+{
+    /// <summary>
+    /// Интерфейс для таймера, позволяющего начать обратный отсчёт и останавливаться.
+    /// </summary>
+    public interface IRaceTimer
+    {
+        /// <summary>
+        /// Событие, срабатывающее каждые заданные интервалы времени.
+        /// </summary>
+        event EventHandler<EventArgs> Tick;
+
+        /// <summary>
+        /// Начинает работу таймера.
+        /// </summary>
+        void Start();
+
+        /// <summary>
+        /// Останавливает таймер.
+        /// </summary>
+        void Stop();
+    }
+}
