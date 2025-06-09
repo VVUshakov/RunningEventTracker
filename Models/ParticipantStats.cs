2bc7d4ca482625b4fbe71d1cb98ed42cfd6b4c6d
Владимир Ушаков <svayznoy@inbox.ru>
2025-06-09 19:32:48 +0700

    Реализовать модель хранящую статистику по каждому участнику

---
+++ Models/ParticipantStats.cs
@@ -0,0 +1,28 @@
+﻿namespace RunningEventTracker.Models
+{
+    /// <summary>
+    /// Содержит агрегированные показатели по одному участнику.
+    /// </summary>
+    public class ParticipantStats
+    {
+        /// <summary>
+        /// Идентификатор участника.
+        /// </summary>
+        public int Participant { get; set; }
+
+        /// <summary>
+        /// Количество завершённых кругов.
+        /// </summary>
+        public int LapCount { get; set; }
+
+        /// <summary>
+        /// Общая сумма секунд, затраченных на участие.
+        /// </summary>
+        public double TotalSeconds { get; set; }
+
+        /// <summary>
+        /// Среднее время прохождения одного круга в секундах.
+        /// </summary>
+        public double AvgTime { get; set; }
+    }
+}
