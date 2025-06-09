9e807521bc2c8ba51a90dbda020c9deb376e72e8
Владимир Ушаков <svayznoy@inbox.ru>
2025-06-09 19:40:40 +0700

    Реализовать интерфейс для логирования происходящих событий

---
+++ Services/IActionHistory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+
+namespace RunningEventTracker.Services
+{
+    /// <summary>
+    /// Интерфейс для хранилища истории событий.
+    /// </summary>
+    public interface IActionHistory
+    {
+        /// <summary>
+        /// Записывает новое сообщение в журнал.
+        /// </summary>
+        void Log(string message);
+
+        /// <summary>
+        /// Предоставляет список недавних сообщений.
+        /// </summary>
+        IEnumerable<string> RecentLogs();
+    }
+}
